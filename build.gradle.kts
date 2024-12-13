c6d71dfb32fafef733fdd2bf29a61419dd1bb98f
modmuss50 <modmuss50@gmail.com>
2024-12-13 09:36:09 +0000

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "me.modmuss50"
-version = "0.8.1"
+version = "0.8.2"
 description = "The Mod Publish Plugin is a plugin for the Gradle build system to help upload artifacts to a range of common destinations."
 
 repositories {
